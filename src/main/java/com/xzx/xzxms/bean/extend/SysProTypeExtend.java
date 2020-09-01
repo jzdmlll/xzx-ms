9b576cc4e37a5be3098918a4b8a199b73af97745
Sunny <cherishlun@outlook.com>
2020-09-01 20:53:45 +0800

    sunny
    2020/9/1

---
+++ src/main/java/com/xzx/xzxms/bean/extend/SysProTypeExtend.java
@@ -0,0 +1,8 @@
+package com.xzx.xzxms.bean.extend;
+
+import com.xzx.xzxms.bean.SysProType;
+
+public class SysProTypeExtend extends SysProType {
+
+
+}
---
+++ src/main/java/com/xzx/xzxms/web/controller/SysProTypeController.java
@@ -0,0 +1,36 @@
+package com.xzx.xzxms.web.controller;
+import com.xzx.xzxms.bean.SysProType;
+import com.xzx.xzxms.service.ISysProTypeService;
+import com.xzx.xzxms.utils.Message;
+import com.xzx.xzxms.utils.MessageUtil;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/project/proType")
+public class SysProTypeController {
+
+    @Resource
+    private ISysProTypeService sysProTypeServiceImpl;
+
+    @ApiOperation(value = "保存或更新")
+    @PostMapping(value = "saveOrUpdate")
+    public Message saveOrUpdate(SysProType sysProType){
+
+        sysProTypeServiceImpl.saveOrUpdate(sysProType);
+        return MessageUtil.success("更新成功");
+    }
+
+    @ApiOperation(value = "查询")
+    @GetMapping(value="/project/findAll")
+    public Message findAll(){
+        List<SysProType> list= sysProTypeServiceImpl.findAll();
+        return MessageUtil.success("查询成功",list);
+    }
+}
