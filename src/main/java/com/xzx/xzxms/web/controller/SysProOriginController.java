d50d8af2bca3a9b2c345926b4c900af3dc55701f
jzdmlll <1037617790@qq.com>
2020-09-02 16:54:28 +0800

    lzc-feature-重构数据库，项目来源功能

--- src/main/java/com/xzx/xzxms/web/controller/SysProOriginController.java
+++ src/main/java/com/xzx/xzxms/web/controller/SysProOriginController.java
@@ -5,6 +5,7 @@ import com.xzx.xzxms.service.ISysProOriginService;
 import com.xzx.xzxms.utils.Message;
 import com.xzx.xzxms.utils.MessageUtil;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,16 @@ public class SysProOriginController {
     @GetMapping("/findAll")
     public Message findAll() {
         List<SysProOrigin> proOriginList = sysProOriginServiceImpl.findAll();
-        return MessageUtil.success("success", proOriginList);
+        return MessageUtil.success("操作成功", proOriginList);
+    }
+    @PostMapping("/saveOrUpdate")
+    public Message saveOrUpdate(SysProOrigin proOrigin) {
+        sysProOriginServiceImpl.saveOrUpdate(proOrigin);
+        return MessageUtil.success("操作成功");
+    }
+    @PostMapping("/logicDeleteById")
+    public Message logicDeleteById(int proOriginId) {
+        sysProOriginServiceImpl.logicDeleteById(proOriginId);
+        return MessageUtil.success("操作成功");
     }
 }
