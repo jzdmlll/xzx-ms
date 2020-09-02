25a4c124abec1626a7d0858c1ba8f2d2f2bed0f7
jzdmlll <1037617790@qq.com>
2020-09-02 09:17:59 +0800

    lzc-feature-20200902

--- src/main/java/com/xzx/xzxms/config/JwtInterceptor.java
+++ src/main/java/com/xzx/xzxms/config/JwtInterceptor.java
@@ -30,6 +30,7 @@ public class JwtInterceptor extends HandlerInterceptorAdapter {
         final String token = request.getHeader(JwtTokenUtil.AUTH_HEADER_KEY);
         if(StringUtils.isEmpty(token)){
             //throw new UnAuthorizedException("用户还未登录");
+            System.out.println("用户还未登录");
             return true;
         }
         // 验证token是否有效--无效已做异常抛出，由全局异常处理后返回对应信息
