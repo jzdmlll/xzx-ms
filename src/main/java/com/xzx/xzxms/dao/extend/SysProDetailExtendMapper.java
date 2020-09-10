e7efe0382a88bb343b030550eea8aade1311350f
Sunny <cherishlun@outlook.com>
2020-09-10 10:51:15 +0800

    2020.9.10 sunny

--- src/main/java/com/xzx/xzxms/dao/extend/SysProDetailExtendMapper.java
+++ src/main/java/com/xzx/xzxms/dao/extend/SysProDetailExtendMapper.java
@@ -8,16 +8,5 @@ import java.util.List;
 public interface SysProDetailExtendMapper {
 
     SysProDetailExtend findById(long proDetailId);
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> ac1a1f1184bbaef44129ece72994715911589ce2
-=======
-
->>>>>>> a7352bdc1cd51076843a8a490f6672ec4b2b55bc
->>>>>>> dev
     List<SysProDetailExtend> findById();
 }
