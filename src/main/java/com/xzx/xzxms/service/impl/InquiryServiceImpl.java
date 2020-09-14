b252d0f9ab046997a05a0e37e142de8a8ed2b3c3
Sunny <cherishlun@outlook.com>
2020-09-14 21:13:38 +0800

    2020.9.14  sunny  询价模块

--- src/main/java/com/xzx/xzxms/service/impl/InquiryServiceImpl.java
+++ src/main/java/com/xzx/xzxms/service/impl/InquiryServiceImpl.java
@@ -2,9 +2,12 @@ package com.xzx.xzxms.service.impl;
 
 import com.xzx.xzxms.bean.Inquiry;
 import com.xzx.xzxms.bean.InquiryExample;
+import com.xzx.xzxms.bean.InquiryWithBLOBs;
 import com.xzx.xzxms.dao.InquiryMapper;
 import com.xzx.xzxms.service.IInquiryService;
+import com.xzx.xzxms.utils.IDUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -21,5 +24,37 @@ public class InquiryServiceImpl implements IInquiryService {
         InquiryExample inquiryExample=new InquiryExample();
         inquiryExample.createCriteria().andProDetailIdEqualTo(proId);
         return inquiryMapper.selectByExample(inquiryExample);
+    }
+
+    @Override
+    public void saveOrUpdate(InquiryWithBLOBs inquiry) {
+
+        if (inquiry.getId() != null){
+            inquiryMapper.updateByPrimaryKey(inquiry);
+        }else {
+            inquiry.setId(IDUtils.getId());
+            inquiryMapper.insert(inquiry);
+        }
+    }
+
+    @Transactional
+    @Override
+    public void setInvalid(long[] ids) {
+
+        InquiryWithBLOBs inquiry=new InquiryWithBLOBs();
+        for (long id:ids) {
+            inquiry.setId(id);
+            inquiry.setIsActive(0);
+            inquiryMapper.updateByPrimaryKeySelective(inquiry);
+        }
+    }
+
+    @Transactional
+    @Override
+    public void delete(long[] ids) {
+
+        for (long id:ids) {
+            inquiryMapper.deleteByPrimaryKey(id);
+        }
     }
 }
