fab455c2a5c0a36c87b35d595d8a8809c045eb15
jzdmlll <1037617790@qq.com>
2020-09-17 15:52:28 +0800

    lzc-20200917-文件上传

--- src/main/java/com/xzx/xzxms/service/impl/SysProCheckServiceImpl.java
+++ src/main/java/com/xzx/xzxms/service/impl/SysProCheckServiceImpl.java
@@ -1,8 +1,20 @@
 package com.xzx.xzxms.service.impl;
 
+import com.xzx.xzxms.bean.SysProCheck;
+import com.xzx.xzxms.bean.SysProDetail;
 import com.xzx.xzxms.service.ISysProCheckService;
 
 public class SysProCheckServiceImpl implements ISysProCheckService {
+
+    @Override
+    public void proDetailInsert(SysProDetail sysProDetail) {
+
+    }
+
+    @Override
+    public void inquiryInsert(SysProCheck sysProCheck) {
+
+    }
 
     @Override
     public void approved(long[] ids) {
