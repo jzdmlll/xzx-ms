d3705161b69840e65b0fb2a12aba4ac1537180c3
孙乃裕 <cherishlun@outlook.com>
2020-10-26 15:56:07 +0800

    新增审核提交、报价单保存审核接口

---
+++ src/main/java/com/xzx/xzxms/service/IProCheckService.java
@@ -0,0 +1,24 @@
+package com.xzx.xzxms.service;
+
+import com.xzx.xzxms.bean.SysProCheck;
+
+import java.util.List;
+
+public interface IProCheckService {
+
+    /**
+     * 报价单上传解析后需同时将报价信息根据审核流程插入到审核表中
+     * @param type 审核类型
+     * @param contentIds 询价ID
+     * @param operator 操作人
+     */
+    void InsertQuoteToCheck(String type, List<Long> contentIds,long operator);
+
+    /**
+     * 改变审核状态
+     * @param status 审核状态
+     * @param sysProChecks 审核内容对象集合
+     * @param operator 操作人
+     */
+    void UpdateCheckStatus(Integer status, List<SysProCheck> sysProChecks, long operator);
+}
