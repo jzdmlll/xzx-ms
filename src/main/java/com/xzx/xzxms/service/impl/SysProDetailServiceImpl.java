bf273b5ef6dad3b432c3cf3e38715ac90828a01f
孙乃裕 <cherishlun@outlook.com>
2020-10-27 12:51:55 +0800

    2020/10/27 修改询价函上传后的数据保存

--- src/main/java/com/xzx/xzxms/service/impl/SysProDetailServiceImpl.java
+++ src/main/java/com/xzx/xzxms/service/impl/SysProDetailServiceImpl.java
@@ -11,6 +11,7 @@ import com.xzx.xzxms.dao.redis.JedisDao;
 import com.xzx.xzxms.service.IFileUploadService;
 import com.xzx.xzxms.service.ISysProDetailService;
 import com.xzx.xzxms.utils.Base64Util;
+import com.xzx.xzxms.utils.CustomerException;
 import com.xzx.xzxms.utils.IDUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -42,92 +43,79 @@ public class SysProDetailServiceImpl implements ISysProDetailService {
 
     @Override
     public List<SysProDetailExtend> findById() { return sysProDetailExtendMapper.findById(); }
+
     @Transactional
     @Override
-    public void saveOrUpdate(SysProDetailWithBLOBs proDetail, List<SysFile> files, List<SysProDetailCheck> proChecks) {
+    public void saveOrUpdate(SysProDetailWithBLOBs proDetail, List<SysFile> files, List<SysProDetailCheck> proChecks) throws CustomerException {
         long time = new Date().getTime();
         long operatorId = proDetail.getOperator();
+
+        /**
+         * 项目详情ID不存在则说明该项目第一次导入；若ID存在，则需要将之前的信息删除再重新插入
+         */
         if (proDetail.getId() != null){
             long proDetailId = proDetail.getId();
-            sysProDetailMapper.updateByPrimaryKeySelective(proDetail);
+            SysProDetailWithBLOBs sysProDetailWithBLOBs;
+            sysProDetailWithBLOBs=sysProDetailMapper.selectByPrimaryKey(proDetailId);
+            //如果getIsUseful标志为0代表未使用过，则数据可删除修改，反之不能
+            if (sysProDetailWithBLOBs !=null && sysProDetailWithBLOBs.getIsUseful().equals(0)){
 
-            if (proChecks.size() >0 ){
-                SysProDetailCheckExample example = new SysProDetailCheckExample();
-                example.createCriteria().andProDetailIdEqualTo(proDetailId);
-                sysProDetailCheckMapper.deleteByExample(example);
-            }
-
-            for(SysProDetailCheck check : proChecks) {
-                check.setId(IDUtils.getId());
-                check.setProDetailId(proDetailId);
-                check.setTime(time);
-                check.setCheckStatus(0);
-                check.setOperator(operatorId);
-                sysProDetailCheckMapper.insert(check);
-            }
-
-            sysProDetailMapper.updateByPrimaryKeySelective(proDetail);
-
-            if (proChecks.size() >0 ){
-                SysProDetailCheckExample example = new SysProDetailCheckExample();
-                example.createCriteria().andProDetailIdEqualTo(proDetail.getId());
-                sysProDetailCheckMapper.deleteByExample(example);
-            }
-
-            for(SysProDetailCheck check : proChecks) {
-                sysProDetailCheckMapper.insert(check);
-            }
-
-        }else {
-            long proDetailId = IDUtils.getId();
-            //文件上传
-            for (SysFile file : files) {
-                //如果redis中存在该文件
-                if(jedisDaoImpl.exists(file.getId().toString())) {
-                    //从redis中取出base64文件码
-                    String base64File = jedisDaoImpl.get(file.getId().toString());
-                    //解码，还原成输入流
-                    InputStream inputStream = Base64Util.decodeBase64File(base64File);
-                    System.out.println("redis");
-                    //清除redis该文件缓存
-                    jedisDaoImpl.del(file.getId().toString());
-                    //上传到Nginx
-                    Map<String, Object> map = fileUploadServiceImpl.uploadByStream(inputStream, file.getName());
-
-                    System.out.println(map.get("url"));
-                    //文件信息持久化到数据库
-                    file.setType(SysFileExtend.TYPE_PRODETAIL);
-                    file.setOtherId(proDetailId);
-                    file.setTime(time);
-                    file.setIsActive(1);
-                    file.setIsUseful(1);
-                    file.setOperator(operatorId);
-                    sysFileMapper.insert(file);
-                    System.out.println("数据库");
-                }
-            }
-            //项目信息插入数据库
-            proDetail.setId(proDetailId);
-            proDetail.setTime(time);
-            proDetail.setIsActive(1);
-            proDetail.setIsUseful(0);
-            sysProDetailMapper.insert(proDetail);
-
-            //审核信息插入数据库
-            for (SysProDetailCheck proDetailCheck : proChecks) {
-                proDetailCheck.setCheckStatus(0);
-                proDetailCheck.setProDetailId(proDetailId);
-                proDetailCheck.setTime(time);
-                proDetailCheck.setId(IDUtils.getId());
-                proDetailCheck.setOperator(operatorId);
-                sysProDetailCheckMapper.insert(proDetailCheck);
+                //先通过ID删除项目详情内容,同时删除审核表中相关数据
+                sysProDetailMapper.deleteByPrimaryKey(proDetailId);
+                sysProDetailCheckMapper.deleteByPrimaryKey(proDetailId);
+            }else {
+                throw new CustomerException("此项目已有后续操作，不能再更改项目信息!");
             }
         }
+
+        long proDetailId = IDUtils.getId();
+        //文件上传
+        for (SysFile file : files) {
+            //如果redis中存在该文件
+            if(jedisDaoImpl.exists(file.getId().toString())) {
+                //从redis中取出base64文件码
+                String base64File = jedisDaoImpl.get(file.getId().toString());
+                //解码，还原成输入流
+                InputStream inputStream = Base64Util.decodeBase64File(base64File);
+                System.out.println("redis");
+                //清除redis该文件缓存
+                jedisDaoImpl.del(file.getId().toString());
+                //上传到Nginx
+                Map<String, Object> map = fileUploadServiceImpl.uploadByStream(inputStream, file.getName());
+
+                System.out.println(map.get("url"));
+                //文件信息持久化到数据库
+                file.setType(SysFileExtend.TYPE_PRODETAIL);
+                file.setOtherId(proDetailId);
+                file.setTime(time);
+                file.setIsActive(1);
+                file.setIsUseful(1);
+                file.setOperator(operatorId);
+                sysFileMapper.insert(file);
+                System.out.println("数据库");
+            }
+        }
+        //项目信息插入数据库
+        proDetail.setId(proDetailId);
+        proDetail.setTime(time);
+        proDetail.setIsActive(1);
+        proDetail.setIsUseful(0);
+        sysProDetailMapper.insert(proDetail);
+
+        //审核信息插入数据库
+        for (SysProDetailCheck proDetailCheck : proChecks) {
+            proDetailCheck.setCheckStatus(0);
+            proDetailCheck.setProDetailId(proDetailId);
+            proDetailCheck.setTime(time);
+            proDetailCheck.setId(IDUtils.getId());
+            proDetailCheck.setOperator(operatorId);
+            sysProDetailCheckMapper.insert(proDetailCheck);
+        }
     }
+
 
     @Override
     public SysProDetailExtend findById(long proDetailId) {
         return sysProDetailExtendMapper.findById(proDetailId);
     }
-
 }
