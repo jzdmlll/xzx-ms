d85a25c321367017292d8814d4795000181f81ad
jzdmlll <1037617790@qq.com>
2020-12-15 10:43:11 +0800

    2020/11/15 lzc 开发和测试环境不需要加权限

--- xzxms-boot/src/main/java/com/xzx/xzxms/config/WebConfig.java
+++ xzxms-boot/src/main/java/com/xzx/xzxms/config/WebConfig.java
@@ -1,10 +1,13 @@
 package com.xzx.xzxms.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -16,6 +19,8 @@ public class WebConfig implements WebMvcConfigurer {
 	public JwtInterceptor jwtInterceptor() {
 		return new JwtInterceptor();
 	}
+	@Value("${webconfig.test}")
+	private Boolean test;
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 
@@ -28,14 +33,23 @@ public class WebConfig implements WebMvcConfigurer {
 	}
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		//拦截路径可自行配置多个 可用 ，分隔开
-		registry.addInterceptor(jwtInterceptor())
+		System.out.println(test);
+		if(test) {
+			//拦截路径可自行配置多个 可用 ，分隔开
+			registry.addInterceptor(jwtInterceptor())
+					.addPathPatterns("/**")
+					.excludePathPatterns(
+							"/**/**");
+		}else {
+			//拦截路径可自行配置多个 可用 ，分隔开
+			registry.addInterceptor(jwtInterceptor())
 //				.addPathPatterns("/category/**","/article/**","/user/**","/role/**","/privilege/**")
-				.addPathPatterns("/**")
-				.excludePathPatterns(
-						"/swagger-resources/**","/v2/**","/swagger-ui.html","/webjars/**",
-						"/user/login","/user/logout","user/findMenuByUserIdUsingGET","/user/getCode",
-						"/user/register","/file/upload","/file/uploadCache","/Room/*","/inquiry/test");
+					.addPathPatterns("/**")
+					.excludePathPatterns(
+							"/swagger-resources/**","/v2/**","/swagger-ui.html","/webjars/**",
+							"/user/login","/user/logout","user/findMenuByUserIdUsingGET","/user/getCode",
+							"/user/register","/file/upload","/file/uploadCache","/Room/*");
+		}
 	}
 
 }
