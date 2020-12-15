81187b730eb571df7680a9206a00f147892ca4ba
jzdmlll <1037617790@qq.com>
2020-12-15 16:37:24 +0800

    2020/11/15 lzc 增加日志模块

---
+++ xzxms-commons/src/main/java/com/xzx/xzxms/commons/model/base/service/BaseCommonService.java
@@ -0,0 +1,34 @@
+package com.xzx.xzxms.commons.model.base.service;
+
+import com.xzx.xzxms.system.bean.SysLog;
+import com.xzx.xzxms.system.bean.SysUser;
+
+/**
+ * common接口
+ */
+public interface BaseCommonService {
+
+    /**
+     * 保存日志
+     * @param log
+     */
+    void addLog(SysLog log);
+
+    /**
+     * 保存日志
+     * @param LogContent
+     * @param logType
+     * @param operateType
+     * @param user
+     */
+    void addLog(String LogContent, Integer logType, Integer operateType, SysUser user);
+
+    /**
+     * 保存日志
+     * @param LogContent
+     * @param logType
+     * @param operateType
+     */
+    void addLog(String LogContent, Integer logType, Integer operateType);
+
+}
