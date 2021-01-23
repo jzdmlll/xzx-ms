a27e64ec2477da573779d1e40623abe616b5c5d2
孙乃裕 <cherishlun@outlook.com>
2021-01-23 20:13:24 +0800

    2021/01/23 sunny 询价模板增删改查

---
+++ xzxms-inquiry/inquiry-service/src/main/java/com/xzx/xzxms/inquiry/service/impl/InquiryTemplateServiceImpl.java
@@ -0,0 +1,59 @@
+package com.xzx.xzxms.inquiry.service.impl;/**
+ * @Author sunny
+ * @Date 2021/1/23 19:45
+ * @Version 1.0
+ */
+
+import com.xzx.xzxms.commons.utils.IDUtils;
+import com.xzx.xzxms.inquiry.bean.InquiryTemplate;
+import com.xzx.xzxms.inquiry.bean.InquiryTemplateExample;
+import com.xzx.xzxms.inquiry.bean.InquiryTemplateWithBLOBs;
+import com.xzx.xzxms.inquiry.dao.InquiryTemplateMapper;
+import com.xzx.xzxms.inquiry.dao.extend.InquiryTemplateExtendMapper;
+import com.xzx.xzxms.inquiry.service.IInquiryTemplateService;
+import io.swagger.models.auth.In;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *@ClassName InquiryTemplateServiceImpl
+ *@Description TODO
+ *@Author sunny
+ *@Date DATE{TIME}
+ */
+public class InquiryTemplateServiceImpl implements IInquiryTemplateService {
+
+    @Resource
+    private InquiryTemplateMapper inquiryTemplateMapper;
+    @Resource
+    private InquiryTemplateExtendMapper inquiryTemplateExtendMapper;
+
+    @Override
+    public List<InquiryTemplateWithBLOBs> findInquiryTemplate(Long id) {
+
+        return inquiryTemplateExtendMapper.findAll(id);
+    }
+
+    @Override
+    public void insertInquiryTemplate(InquiryTemplateWithBLOBs inquiryTemplateWithBLOBs) {
+
+        inquiryTemplateWithBLOBs.setId(IDUtils.getId());
+        inquiryTemplateWithBLOBs.setTime(new Date().getTime());
+        inquiryTemplateMapper.insertSelective(inquiryTemplateWithBLOBs);
+    }
+
+    @Override
+    public void updateInquiryTemplate(InquiryTemplateWithBLOBs inquiryTemplateWithBLOBs) {
+
+        inquiryTemplateWithBLOBs.setTime(new Date().getTime());
+        inquiryTemplateMapper.updateByPrimaryKeyWithBLOBs(inquiryTemplateWithBLOBs);
+    }
+
+    @Override
+    public void setInvalidInquiryTemplate(InquiryTemplate inquiryTemplate) {
+
+        inquiryTemplateMapper.deleteByPrimaryKey(inquiryTemplate.getId());
+    }
+}
