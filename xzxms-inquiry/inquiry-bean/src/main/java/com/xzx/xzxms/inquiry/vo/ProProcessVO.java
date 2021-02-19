98b958c80f88dcb279866be41e126df1d608559c
孙乃裕 <cherishlun@outlook.com>
2021-02-19 16:05:04 +0800

    2021/02/19 sunny index首页项目流程进度查询接口

---
+++ xzxms-inquiry/inquiry-bean/src/main/java/com/xzx/xzxms/inquiry/vo/ProProcessVO.java
@@ -0,0 +1,123 @@
+package com.xzx.xzxms.inquiry.vo;/**
+ * @Author sunny
+ * @Date 2021/2/19 14:23
+ * @Version 1.0
+ */
+
+/**
+ *@ClassName ProProcessVO
+ *@Description TODO
+ *@Author sunny
+ *@Date DATE{TIME}
+ */
+public class ProProcessVO {
+
+    private Long proId;
+    private String proName;
+    private Long iNum;
+    private Long qNum;
+    private Long ttNum;
+    private Long btNum;
+    private Long ctNum;
+    private Long ftNum;
+    private Long tNum;
+    private Long bNum;
+    private Long cNum;
+    private Long fNum;
+
+    public Long getProId() {
+        return proId;
+    }
+
+    public void setProId(Long proId) {
+        this.proId = proId;
+    }
+
+    public String getProName() {
+        return proName;
+    }
+
+    public void setProName(String proName) {
+        this.proName = proName;
+    }
+
+    public Long getiNum() {
+        return iNum;
+    }
+
+    public void setiNum(Long iNum) {
+        this.iNum = iNum;
+    }
+
+    public Long getqNum() {
+        return qNum;
+    }
+
+    public void setqNum(Long qNum) {
+        this.qNum = qNum;
+    }
+
+    public Long getTtNum() {
+        return ttNum;
+    }
+
+    public void setTtNum(Long ttNum) {
+        this.ttNum = ttNum;
+    }
+
+    public Long getBtNum() {
+        return btNum;
+    }
+
+    public void setBtNum(Long btNum) {
+        this.btNum = btNum;
+    }
+
+    public Long getCtNum() {
+        return ctNum;
+    }
+
+    public void setCtNum(Long ctNum) {
+        this.ctNum = ctNum;
+    }
+
+    public Long getFtNum() {
+        return ftNum;
+    }
+
+    public void setFtNum(Long ftNum) {
+        this.ftNum = ftNum;
+    }
+
+    public Long gettNum() {
+        return tNum;
+    }
+
+    public void settNum(Long tNum) {
+        this.tNum = tNum;
+    }
+
+    public Long getbNum() {
+        return bNum;
+    }
+
+    public void setbNum(Long bNum) {
+        this.bNum = bNum;
+    }
+
+    public Long getcNum() {
+        return cNum;
+    }
+
+    public void setcNum(Long cNum) {
+        this.cNum = cNum;
+    }
+
+    public Long getfNum() {
+        return fNum;
+    }
+
+    public void setfNum(Long fNum) {
+        this.fNum = fNum;
+    }
+}
